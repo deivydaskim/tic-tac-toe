312353bc3c19088570cfa7a080b0dff8e242ea98
deivydas.kim <deivydas.kim@gmail.com>
2024-06-13 14:53:34 +0300

    create game board

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Player from './components/Player';
+import GameBoard from './components/GameBoard';
 
 function App() {
-  
+
   return (
     <main>
       <div id="game-container">
@@ -9,7 +10,7 @@ function App() {
           <Player name={'Player 1'} symbol={'X'} />
           <Player name={'Player 2'} symbol={'O'} />
         </ol>
-        GAME BOARD
+        <GameBoard />
       </div>
       LOG
     </main>
